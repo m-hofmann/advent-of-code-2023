9fa455b43fc5f04b7ce72198be5c847008c78b42
Martin Hofmann <m-hofmann@users.noreply.github.com>
2023-12-13 20:26:11 +0100

    Day 12 part 2 solution

--- 12/12.go
+++ 12/12.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -35,41 +35,109 @@ func main() {
 
 	sum := 0
 	for _, record := range records {
-		sum += getPermCount(record.report, record.checksums)
+		sum += memoizedGetPermCount(record)
 	}
 
 	fmt.Println("Part 1 solution:", sum)
 
+	sum = 0
+	for _, curr := range records {
+		report := fmt.Sprintf("%s?%s?%s?%s?%s", string(curr.report), string(curr.report), string(curr.report), string(curr.report), string(curr.report))
+		checksums := make([]int, len(curr.checksums)*5)
+		for i := 0; i < len(checksums); i++ {
+			checksums[i] = curr.checksums[i%len(curr.checksums)]
+		}
+		sum += memoizedGetPermCount(record{[]byte(report), checksums})
+	}
+
+	fmt.Println("Part 2 solution:", sum)
+
 }
 
-func getPermCount(report []byte, checksums []int) int {
-	perms := make([][]byte, 0)
-	perms = append(perms, report)
-	for i := 0; i < len(report); i++ {
-		newPerms := make([][]byte, 0)
-		for _, perm := range perms {
-			if perm[i] == '?' {
-				for _, c := range []byte{'.', '#'} {
-					tmp := make([]byte, len(perm))
-					copy(tmp, perm)
-					tmp[i] = c
-					newPerms = append(newPerms, tmp)
-				}
-			} else {
-				newPerms = append(newPerms, perm)
-			}
-		}
-		perms = newPerms
+var cache = make(map[string]int)
 
+func memoizedGetPermCount(r record) int {
+	// Golang memoization with arbitrary structs (possible containing slices) is a pain
+	// this must be sooo inefficient
+	key := string(r.report) + "/" + fmt.Sprintf("%v", r.checksums)
+	if _, ok := cache[key]; !ok {
+		cache[key] = getPermCount(r)
+		return cache[key]
+	} else {
+		return cache[key]
 	}
+}
 
-	valid := 0
-	for _, perm := range perms {
-		if reflect.DeepEqual(getChecksum(perm), checksums) {
-			valid++
+func getPermCount(r record) int {
+	// took the liberty of brushing up on my dynamic programming using this tutorial
+	// https://old.reddit.com/r/adventofcode/comments/18hbbxe/2023_day_12python_stepbystep_tutorial_with_bonus/
+	if len(r.checksums) == 0 {
+		if !slices.Contains(r.report, '#') {
+			return 1
+		} else {
+			return 0
 		}
 	}
-	return valid
+
+	if len(r.report) == 0 {
+		return 0
+	}
+
+	nxt := r.report[0]
+	out := 0
+	switch nxt {
+	case '.':
+		out = handleDot(r.report, r.checksums)
+	case '#':
+		out = handleHash(r.report, r.checksums)
+	case '?':
+		out = handleDot(r.report, r.checksums) + handleHash(r.report, r.checksums)
+	}
+
+	// fmt.Println(string(report), checksums, "->", out)
+
+	return out
+}
+
+func handleDot(report []byte, checksums []int) int {
+	return memoizedGetPermCount(record{report[1:], checksums})
+}
+
+func handleHash(report []byte, checksums []int) int {
+	nextChecksum := checksums[0]
+	currSprings := make([]byte, nextChecksum)
+	hashCount := 0
+	if len(report) < nextChecksum {
+		return 0
+	}
+	for i, c := range report[:nextChecksum] {
+		if c == '?' {
+			currSprings[i] = '#'
+			hashCount++
+		} else if c == '#' {
+			currSprings[i] = c
+			hashCount++
+		} else {
+			currSprings[i] = c
+		}
+	}
+	if hashCount != nextChecksum {
+		return 0
+	}
+
+	if len(report) == nextChecksum {
+		// last n bytes fits last checksum, we're done
+		if len(checksums) == 1 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	if report[nextChecksum] == '?' || report[nextChecksum] == '.' {
+		// can be separator, skip and go to next group
+		return memoizedGetPermCount(record{report[nextChecksum+1:], checksums[1:]})
+	}
+	return 0
 }
 
 func getChecksum(report []byte) []int {
