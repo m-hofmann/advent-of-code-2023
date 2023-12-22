b2ba91a5e7345276d7108798420309ba0fa67469
Martin Hofmann <m-hofmann@users.noreply.github.com>
2023-12-22 16:54:12 +0100

    Add day 19 part 2 solution

--- 19/19.go
+++ 19/19.go
@@ -23,6 +23,52 @@ type part struct {
 	values map[byte]int
 }
 
+// start and end inclusive
+type interval struct {
+	start int
+	end   int
+}
+
+type state struct {
+	workflow string
+	ranges   map[byte]interval
+}
+
+func (s *state) copy() state {
+	newState := state{s.workflow, make(map[byte]interval)}
+	for k, v := range s.ranges {
+		newState.ranges[k] = v
+	}
+	return newState
+}
+
+func (s *state) assertInvariants() {
+	for _, r := range s.ranges {
+		if r.end < r.start {
+			log.Fatalln("Invalid range", s)
+		}
+	}
+}
+
+func (s *state) getRangeCoverage(name byte) int64 {
+	// +1 to compensate for the fact that all start, end are inclusive
+	return int64(s.ranges[name].end - s.ranges[name].start + 1)
+}
+
+func (s *state) getCombinations() int64 {
+	return s.getRangeCoverage('x') * s.getRangeCoverage('m') *
+		s.getRangeCoverage('a') * s.getRangeCoverage('s')
+}
+
+func (s *state) String() string {
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("@%s:", s.workflow))
+	for k, v := range s.ranges {
+		sb.WriteString(fmt.Sprintf("%s -> [%4d, %4d], ", k, v.start, v.end))
+	}
+	return sb.String()
+}
+
 func main() {
 	fmt.Println("Starting day 19 ... ")
 
@@ -33,6 +79,146 @@ func main() {
 	defer f.Close()
 
 	sc := bufio.NewScanner(f)
+	workflows, parts := parseInput(sc)
+
+	allAcceptedValues := getAcceptedPartValues(parts, workflows)
+	fmt.Printf("Part 1 solution %d\n", allAcceptedValues)
+
+	finalAcceptedRanges := make([]state, 0)
+	finalRejectedRanges := make([]state, 0)
+	unfinished := make([]state, 1)
+	unfinished[0] = state{
+		"in",
+		map[byte]interval{
+			'x': {1, 4000},
+			'm': {1, 4000},
+			'a': {1, 4000},
+			's': {1, 4000},
+		}}
+
+	for len(unfinished) > 0 {
+		curr := unfinished[0]
+		unfinished = unfinished[1:]
+		curr.assertInvariants()
+		if curr.workflow == "A" {
+			finalAcceptedRanges = append(finalAcceptedRanges, curr)
+			continue
+		} else if curr.workflow == "R" {
+			finalRejectedRanges = append(finalRejectedRanges, curr)
+			continue
+		}
+
+		wf, ok := workflows[curr.workflow]
+		if !ok {
+			log.Fatalln("Could not find workflow", curr)
+		}
+		for _, rule := range wf.rules {
+			currRange := curr.ranges[rule.variable]
+			switch rule.comparator {
+			case '<':
+				if currRange.end < rule.value {
+					// case 1: currRange \in rule range
+					curr.workflow = rule.target
+					unfinished = append(unfinished, curr)
+					goto out
+				} else if currRange.start < rule.value && rule.value < currRange.end {
+					// case 2: overlap - a part matches. Interval must be split
+					second := curr.copy()
+					// create second interval that should have matched rule and moves to new workflow
+					oldRange := second.ranges[rule.variable]
+					oldRange.end = rule.value - 1
+					second.ranges[rule.variable] = oldRange
+					second.workflow = rule.target
+					unfinished = append(unfinished, second)
+
+					oldRange = curr.ranges[rule.variable]
+					oldRange.start = rule.value
+					curr.ranges[rule.variable] = oldRange
+				} else if rule.value <= currRange.start && rule.value <= currRange.end {
+					// case 3: no match -> next rule
+				}
+			case '>':
+				if rule.value < currRange.start && rule.value < currRange.end {
+					// case 1: currRange \in rule range
+					curr.workflow = rule.target
+					unfinished = append(unfinished, curr)
+					goto out
+				} else if currRange.start < rule.value && rule.value < currRange.end {
+					// case 2: overlap - a part matches. Interval must be split
+					second := curr.copy()
+					// create second interval that should have matched rule and moves to new workflow
+					oldRange := second.ranges[rule.variable]
+					oldRange.start = rule.value + 1
+					second.ranges[rule.variable] = oldRange
+					second.workflow = rule.target
+					unfinished = append(unfinished, second)
+
+					oldRange = curr.ranges[rule.variable]
+					oldRange.end = rule.value
+					curr.ranges[rule.variable] = oldRange
+				} else if rule.value > currRange.start && rule.value > currRange.end {
+					// no match -> next rule
+				}
+			default:
+				curr.workflow = rule.target
+				unfinished = append(unfinished, curr)
+				goto out
+			}
+		}
+	out:
+	}
+
+	totalAccCombinations := int64(0)
+
+	for _, acceptedState := range finalAcceptedRanges {
+		localCombinations := acceptedState.getCombinations()
+		totalAccCombinations += localCombinations
+	}
+
+	fmt.Println("Part 2 solution", totalAccCombinations)
+}
+
+func getAcceptedPartValues(parts []part, workflows map[string]workflow) int {
+	allAcceptedValues := 0
+	for _, part := range parts {
+		state := "in"
+
+		for state != "A" && state != "R" {
+			wf, ok := workflows[state]
+			if !ok {
+				log.Fatalln("Could not find workflow for state", state)
+			}
+
+			for _, rule := range wf.rules {
+				switch rule.comparator {
+				case '<':
+					partVal := part.values[rule.variable]
+					if partVal < rule.value {
+						state = rule.target
+						goto out
+					}
+				case '>':
+					partVal := part.values[rule.variable]
+					if partVal > rule.value {
+						state = rule.target
+						goto out
+					}
+				default:
+					state = rule.target
+					goto out
+				}
+			}
+		out:
+		}
+
+		if state == "A" {
+			allAcceptedValues += partValue(part)
+		}
+	}
+	return allAcceptedValues
+}
+
+func parseInput(sc *bufio.Scanner) (map[string]workflow, []part) {
 	workflows := make(map[string]workflow, 0)
 	for sc.Scan() {
 		line := sc.Text()
@@ -75,47 +261,7 @@ func main() {
 		}
 		parts = append(parts, part)
 	}
-
-	allAcceptedValues := 0
-	for _, part := range parts {
-		state := "in"
-		fmt.Println("=== Part", part, "===")
-
-		for state != "A" && state != "R" {
-			fmt.Println("Now in state", state)
-			wf, ok := workflows[state]
-			if !ok {
-				log.Fatalln("Could not find workflow for state", state)
-			}
-
-			for _, rule := range wf.rules {
-				switch rule.comparator {
-				case '<':
-					partVal := part.values[rule.variable]
-					if partVal < rule.value {
-						state = rule.target
-						goto out
-					}
-				case '>':
-					partVal := part.values[rule.variable]
-					if partVal > rule.value {
-						state = rule.target
-						goto out
-					}
-				default:
-					state = rule.target
-					goto out
-				}
-			}
-		out:
-		}
-
-		if state == "A" {
-			allAcceptedValues += partValue(part)
-		}
-	}
-
-	fmt.Printf("Part 1 solution %d\n", allAcceptedValues)
+	return workflows, parts
 }
 
 func partValue(p part) int {
